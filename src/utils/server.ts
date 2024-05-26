21d9a58df4f16d3fa1923a2353871dd26ad2ccf6
Aftab Hussain <aftabh4004@gmail.com>
2024-05-26 13:59:56 +0530

    setup(Applications):Register application 	- Create Application 	- Create Application Handler 	- Setup Default roles, Super_Admin and Application_user

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -1,5 +1,7 @@
 import fastify from "fastify";
 import { logger } from "./logger";
+import { applicationRoutes } from "../modules/applications/applications.routes";
+
 
 export async function build_server(){
     const app =  fastify({
@@ -9,6 +11,11 @@ export async function build_server(){
     //reg plugin
 
     //reg 
+
+    app.register(applicationRoutes, { prefix: "/api/applications" });
+  
+
+
     return app;
 
 }
